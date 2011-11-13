defc758899c2e4644dd0c3454ad36a1ff92f5afb
Ben Lovett <ben@tilderoot.com>
2011-11-12 17:07:03 -0800

    add beer-ware license.

--- split-and-tag.rb
+++ split-and-tag.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
 
+#
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <ben@biggiantnerds.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.
+#    - Ben Lovett
+# ----------------------------------------------------------------------------
+#
+
 require 'optparse'
 
 options = {:cue_file	=> nil,
